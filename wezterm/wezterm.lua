2b110355d345ef705126b2cb528ac7fdf1ccc23d
Achraf <elafrit.achraf@gmail.com>
2024-10-09 05:29:24 +0200

    fix missing homebrew path on mac

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,45 +1,16 @@
 local wezterm = require 'wezterm'
--- local mux = wezterm.mux
---
--- wezterm.on('gui-startup', function(cmd)
-  -- local project_dir = wezterm.home_dir .. '/code/learning_rust'
-  -- local tab, main_pane, window = mux.spawn_window {
-  --   workspace = 'test',
-  --   cwd = project_dir,
-  -- }
---   local second_pane = main_pane:split {
---     direction = 'Right',
---     size = 0.3,
---     cwd = project_dir,
---   }
---
---   --main_pane:send_text 'echo "this is 1st pane"\n'
---   -- second_pane:send_text 'echo "this is 2st pane"\n'
--- end)
---
 
 return {
-	-- color_scheme = 'termnial.sexy',
+ set_environment_variables = {
+  PATH = '/opt/homebrew/bin:/usr/local/bin:' .. os.getenv('PATH')
+},
   window_close_confirmation = 'NeverPrompt',
-  -- default_prog = { 'tmux', 'new', '-s', 'foo', '-c', '~/code/learning_rust/' },
-  default_prog = { 'tmux', 'new', '-c', wezterm.home_dir .. '/code/learning_rust/' },
+  default_prog = { 'tmux', 'new', '-c', wezterm.home_dir .. '/code' },
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 16.0,
-	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
-	
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,y
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
 	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	-- window_decorations = 'RESIZE',
 	keys = {
 		{
 			key = 'f',
@@ -48,7 +19,6 @@ return {
 		},
 	},
 	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
 	  {
 	    event = { Up = { streak = 1, button = 'Left' } },
 	    mods = 'CTRL',
@@ -62,5 +32,3 @@ return {
   bottom = 0,
   },
 }
-
-
