cf25cffed340597a9eb855e0b26a897a2d129707
Achraf <elafrit.achraf@gmail.com>
2025-12-24 12:16:46 +0100

    update packages

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,9 +3,16 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>')
 vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+-- open gitlab pipeline in browser
 vim.keymap.set(
   "n",
   "<leader>gw",
   ":!glab ci view --web<CR>",
   { noremap = true, silent = true, desc = "View Gitlab CI in web" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>gha",
+  ":!gh run view --web (gh run list --branch (git rev-parse --abbrev-ref HEAD) --limit 1 --json databaseId --jq '.[0].databaseId')<CR>",
+  { noremap = true, silent = true, desc = "Open github action pipeline in web browser" }
 )
